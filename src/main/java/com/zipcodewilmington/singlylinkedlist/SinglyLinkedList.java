3f8bf915f6edab3170dd1291453ba3b8d2e4ec10
Loc <loc@Zipcoders-MacBook-Pro-3.local>
2023-11-29 21:22:07 -0500

    generataed the methods, still have to fill them out

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -9,4 +9,31 @@ public class SinglyLinkedList {
 
     public Object[] list;
 
+    public boolean add() {
+        return false;
+    }
+
+    public boolean remove() {
+        return false;
+    }
+
+    public boolean contains() {
+        return false;
+    }
+
+    public int find(Object looking) {
+        return -1;
+    }
+
+    public int size() {
+        return -1;
+    }
+
+    public int get(int location) {
+        return -1;
+    }
+
+    public SinglyLinkedList copy() {
+        return null;
+    }
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -8,31 +8,31 @@ public class SinglyLinkedListTest {
 
     @Test
     public void listAddTest(){
-        Object[] list = new Object[10];
+        SinglyLinkedList list = new SinglyLinkedList();
         Assert.assertTrue(list.add());
     }
 
     @Test
     public void listRemoveTest(){
-        Object[] list = new Object[10];
+        SinglyLinkedList list = new SinglyLinkedList();
         Assert.assertTrue(list.remove());
     }
     @Test
     public void listContainsTest(){
-        Object[] list = new Object[10];
+        SinglyLinkedList list = new SinglyLinkedList();
         Assert.assertTrue(list.contains());
     }
     @Test
     public void listFindTest(){
-        Object[] list = new Object[10];
-        Object expected = list[2];
+        SinglyLinkedList list = new SinglyLinkedList();
+        Object expected = list.list[2];
         Object looking = 2;
         Assert.assertEquals((int)expected, list.find(looking));
     }
 
     @Test
     public void listSizeTest(){
-        Object[] list = new Object[10];
+        SinglyLinkedList list = new SinglyLinkedList();
         int expect = 12;
         int actual = list.size();
         Assert.assertEquals(expect, actual );
@@ -40,7 +40,7 @@ public class SinglyLinkedListTest {
 
     @Test
     public void listGetTest(){
-        Object[] list = new Object[10];
+        SinglyLinkedList list = new SinglyLinkedList();
         int expected = 2;
         int location = 3;
         Assert.assertEquals(expected, list.get(location));
@@ -48,14 +48,14 @@ public class SinglyLinkedListTest {
 
     @Test
     public void listCopyTest(){
-        Object[] list = new Object[10];
-        Object[] newList = list.copy();
+        SinglyLinkedList list = new SinglyLinkedList();
+        SinglyLinkedList newList = list.copy();
         Assert.assertEquals(list, newList);
     }
 
     @Test
-    public void listAddTest(){
-        Object[] list = new Object[10];
+    public void listSortTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
         Assert.assertTrue(list.add());
     }
 }
